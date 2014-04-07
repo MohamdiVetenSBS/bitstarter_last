a69603492796cf4b7a41b40416a69aa1fcb392d8
Mohamdi Veten <mohamdi@ubuntu.(none)>
2014-04-07 05:13:29 -0700

    modification to web.js

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
-var express = require('express');
+ var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
+  fs.readFileSync('index.html', function(data){
+	response.send(data);
+	console.log(data);
+});
   response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
+	var cont = fs.readFileSync('index.html');
+	console.log(cont.toString());
+
   console.log("Listening on " + port);
 });
+
